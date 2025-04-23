2fdaeecb83090c35cc674331306ddec5dbd07413
Kavyansh Barjattya <117171893+kavyanshbarjattya@users.noreply.github.com>
2025-04-23 13:17:02 +0530

    Initial commit

---
+++ Assets/Scripts/BlackPlayerJump.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackPlayerJump : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D _playerBody;
+    bool _isJump;
+    void Update()
+    {
+        Jumping();
+    }
+
+    void Jumping()
+    {
+        if (_isJump)
+        {
+            _playerBody.gravityScale = -_playerBody.gravityScale;
+            _isJump = false;
+        }
+
+    }
+
+    public void Jump()
+    {
+        if (!_isJump)
+        {
+            _isJump = true;
+        }
+        else
+        {
+            _isJump = false;
+        }
+    }
+}
